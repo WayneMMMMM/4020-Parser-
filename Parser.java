cfa2a1fd8733dea79cfb76acaf690d3fabe9f3dd
WayneMMMMM <43655399+WayneMMMMM@users.noreply.github.com>
2018-10-08 13:41:59 -0400

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,5 +1,5 @@
-
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Parser {
@@ -9,17 +9,20 @@ public class Parser {
 	File input = new File ("4020a1-datasets.txt");
 	DatasetPaser myPaser= new DatasetPaser(input);
 	
+
+	
+	
 	APIConnector api = new APIConnector(myPaser.getArticlesMap());
 	api.getXML();
 	
 	Map<String, String> results = api.getIdMap();
 	
-	for (String title : results.keySet()) {
-		String id = results.get(title);
-		System.out.println("Article title: " + title);
-		System.out.println("Article id: " + id);
-		System.out.println("---");
-	}
+
+	
+	XMLWriter myWriter = new XMLWriter("/Users/Wayne/test.txt", results);
+	myWriter.writeFile();
+	
+	
 	
 	}
 }
