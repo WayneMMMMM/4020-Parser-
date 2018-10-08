85a9da829e51cde5e1562f9bc0f1f22229a8269f
borisc1996j <borisc1996j@gmail.com>
2018-10-08 21:32:22 +0100

    remove unused method

--- ArticleParser.java
+++ ArticleParser.java
@@ -6,19 +6,22 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class ArticleParser extends DefaultHandler {
 	
-	// This boolean flag will turn true when the "id" tag is found.
 	boolean idFlag = false;
 	
-	// This variable will hold the ID as a string if found.
 	String id = null;
+	
+	public ArticleParser() {
+		super();
+	}
 	
 	public void startElement(String uri, String name, String qName, Attributes atts) {
 		if(qName.equalsIgnoreCase("Id")) {
 			idFlag = true;
 		}
 	}
-	
+	 
 	public void characters(char ch[], int start, int length) {		
+		
 		if (idFlag) {
 			this.id = new String(ch, start, length);			
 			idFlag = false;
