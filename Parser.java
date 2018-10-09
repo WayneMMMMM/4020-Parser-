e9664ec8cddece6584f5b5c5d2e94c5fe1c27734
WayneMMMMM <43655399+WayneMMMMM@users.noreply.github.com>
2018-10-09 16:01:19 -0400

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -5,20 +5,20 @@ import java.util.Map;
 public class Parser {
 	
 	public static void main(String[] args) {
-		
+	//load dataset	
 	File input = new File ("4020a1-datasets.txt");
+	//parse dataset
 	DatasetParser myPaser= new DatasetParser(input);
 	
 
-	
-	
+
+	//making http requests, get xml from server
 	APIConnector api = new APIConnector(myPaser.getArticlesMap());
 	api.getXML();
-	
 	Map<String, String> results = api.getIdMap();
 	
 
-	
+	//write result to final file
 	XMLWriter myWriter = new XMLWriter("./groupID_result.xml", results);
 	myWriter.writeFile();
 	
